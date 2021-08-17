14de8110c0739e3b29b0780b90fc04be51f91e3f
dawndavidson <84029532+dawndavidson@users.noreply.github.com>
2021-08-17 09:23:11 -0500

    Finished assignment

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,8 +1,10 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
 
 public class BalancedBracketsTest {
 
@@ -12,5 +14,64 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void onlyBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void wordHasNoBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Line"));
+    }
+
+    @Test
+    public void wordHasSymbolsAndOneBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code%"));
+    }
+
+    @Test
+    public void wordHasBracketsFacingTheWrongWay() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Launch [Code"));
+    }
+
+    @Test
+    public void wordHasNoBracketsAndANumber() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Code525"));
+    }
+
+    @Test
+    public void bracketHasAllSpecialCharacters() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("$%^&**(@!"));
+    }
+
+    @Test
+    public void lineOnlyHasBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void lineOnlyHasLetters() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("abcdefghijklmnopqrst"));
+    }
+
+    @Test
+    public void lineHasMultipleBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("L][A[U[N[C[H"));
+    }
+
+    @Test
+    public void lineIsCorrect() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
 }
+
+
+
+
+
+
